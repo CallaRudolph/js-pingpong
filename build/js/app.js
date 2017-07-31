3d7bb71f3158712ad8c149e0621d1c09bbac66b5
Calla Rudolph <callarudolph@gmail.com>
2017-07-30 17:43:59 -0700

    refactor with export for display function - ERROR Weather is not a constructor

--- build/js/app.js
+++ build/js/app.js
@@ -25,6 +25,40 @@ Calculator.prototype.pingPong = function(goal) {
 exports.calculatorModule = Calculator;
 
 },{}],3:[function(require,module,exports){
+var Weather = require('./../js/weather.js').weatherModule;
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city);
+  });
+});
+
+exports.displayHumidityFunction = displayHumidity;
+
+},{"./../js/weather.js":4}],4:[function(require,module,exports){
+var apiKey = require('./../.env').apiKey;
+var displayHumidity = require('./../js/weather-interface.js').displayHumidityFunction;
+
+function Weather() {
+}
+
+Weather.prototype.getWeather = function(city) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+    displayHumidity(city, response.main.humidity);
+  }).fail(function(error) {
+    $('.showWeather').text(error.responseJSON.message);
+  });
+}
+
+exports.weatherModule = Weather;
+
+},{"./../.env":1,"./../js/weather-interface.js":3}],5:[function(require,module,exports){
 var Calculator = require('./../js/pingpong.js').calculatorModule;
 
 $(document).ready(function() {
@@ -52,18 +86,20 @@ $(document).ready(function() {
   $('#time').text(moment());
 });
 
-var apiKey = require('./../.env').apiKey;
+var Weather = require('./../js/weather.js').weatherModule;
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
+}
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
-    });
+    currentWeatherObject.getWeather(city);
   });
 });
 
-},{"./../.env":1,"./../js/pingpong.js":2}]},{},[3]);
+exports.displayHumidityFunction = displayHumidity;
+
+},{"./../js/pingpong.js":2,"./../js/weather.js":4}]},{},[5]);
